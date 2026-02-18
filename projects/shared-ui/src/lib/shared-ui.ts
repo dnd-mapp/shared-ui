2cab891f3427708be5a1b92248ed0770785ac701
Oscar Wellner <oscar.wellner@gmail.com>
2026-02-18 22:48:28 +0100

    Feat: Remove boilerplate storybook stories

--- projects/shared-ui/src/lib/shared-ui.ts
+++
@@ -1,9 +0,0 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-
-@Component({
-    selector: 'dma-shared-ui',
-    template: `<p>shared-ui works!</p>`,
-    changeDetection: ChangeDetectionStrategy.OnPush,
-    imports: [],
-})
-export class SharedUi {}
