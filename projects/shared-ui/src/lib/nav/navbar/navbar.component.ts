750257e894c25b1ef318832df7c63527468d2ade
Oscar Wellner <oscar.wellner@gmail.com>
2026-02-19 22:08:14 +0100

    Feat: Remove unsorted listed usage

--- projects/shared-ui/src/lib/nav/navbar/navbar.component.ts
+++ projects/shared-ui/src/lib/nav/navbar/navbar.component.ts
@@ -3,8 +3,11 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 @Component({
     // eslint-disable-next-line @angular-eslint/component-selector
     selector: 'nav[dma-navbar]',
-    templateUrl: './navbar.component.html',
+    template: `<ng-content />`,
     changeDetection: ChangeDetectionStrategy.OnPush,
+    host: {
+        class: 'flex items-center gap-4',
+    },
     imports: [],
 })
 export class NavbarComponent {}
