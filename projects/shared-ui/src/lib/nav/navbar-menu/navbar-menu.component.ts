8a31693d5abaf5215da0754123429de272b668c3
Oscar Wellner <oscar.wellner@gmail.com>
2026-02-24 00:00:42 +0100

    Feat: Use active marker in navbar menus

--- projects/shared-ui/src/lib/nav/navbar-menu/navbar-menu.component.ts
+++ projects/shared-ui/src/lib/nav/navbar-menu/navbar-menu.component.ts
@@ -1,13 +1,16 @@
 import { booleanAttribute, ChangeDetectionStrategy, Component, input } from '@angular/core';
 import { ButtonComponent } from '../../button';
 import { DropdownAnchorDirective, DropdownContainerComponent } from '../../dropdown';
+import { ActiveMarkerComponent } from '../active-marker/active-marker.component';
 
 @Component({
     selector: 'dma-navbar-menu',
     templateUrl: './navbar-menu.component.html',
     changeDetection: ChangeDetectionStrategy.OnPush,
-    imports: [ButtonComponent, DropdownAnchorDirective, DropdownContainerComponent],
+    imports: [ButtonComponent, DropdownAnchorDirective, DropdownContainerComponent, ActiveMarkerComponent],
 })
 export class NavbarMenuComponent {
+    public readonly label = input.required<string>();
+
     public readonly toggleOnHover = input(false, { transform: booleanAttribute });
 }
