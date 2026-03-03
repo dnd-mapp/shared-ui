08727689c0b0ef90f168e2c77bebddd6f5d8d9cb
Oscar Wellner <oscar.wellner@gmail.com>
2026-03-03 15:34:03 +0100

    Feat: Add solid TrashCan icon

--- projects/shared-ui/src/lib/icons/solid/index.ts
+++ projects/shared-ui/src/lib/icons/solid/index.ts
@@ -1,1 +1,2 @@
 export * from './circle-user/circle-user.so-icon';
+export * from './trash-can/trash-can.icon';
---
+++ projects/shared-ui/src/lib/icons/solid/trash-can/trash-can.so-icon.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+    selector: 'dma-icon[dma-trash-can-so-icon]',
+    templateUrl: './trash-can.so-icon.svg',
+    styleUrl: '../../icon.css',
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    imports: [],
+})
+export class TrashCanSoIcon {}
