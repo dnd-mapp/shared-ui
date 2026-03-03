c73386fcd8348257f0b28d7e1df951515bb42e03
Oscar Wellner <oscar.wellner@gmail.com>
2026-03-03 16:51:01 +0100

    Feat: Add FileLines icons

---
+++ projects/shared-ui/src/lib/icons/regular/file-lines/file-lines.re-icon.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+    selector: 'dma-icon[dma-file-lines-re-icon]',
+    templateUrl: './file-lines.re-icon.svg',
+    styleUrl: '../../icon.css',
+    changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class FileLinesReIcon {}
--- projects/shared-ui/src/lib/icons/regular/index.ts
+++ projects/shared-ui/src/lib/icons/regular/index.ts
@@ -1,6 +1,7 @@
 export * from './clone/clone.re-icon';
 export * from './eye-slash/eye-slash.re-icon';
 export * from './eye/eye.re-icon';
+export * from './file-lines/file-lines.re-icon';
 export * from './file/file.re-icon';
 export * from './floppy-disk/floppy-disk.re-icon';
 export * from './trash-can/trash-can.re-icon';
