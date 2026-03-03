5195d2d401a145fd6dd111b3ee5249c99d86c9d5
Oscar Wellner <oscar.wellner@gmail.com>
2026-03-03 15:39:06 +0100

    Feat: Add solid Xmark icon

--- projects/shared-ui/src/lib/icons/solid/index.ts
+++ projects/shared-ui/src/lib/icons/solid/index.ts
@@ -1,3 +1,4 @@
 export * from './arrow-rotate-left/arrow-rotate-left.so-icon';
 export * from './circle-user/circle-user.so-icon';
-export * from './trash-can/trash-can.icon';
+export * from './trash-can/trash-can.so-icon';
+export * from './xmark/xmark.so-icon';
---
+++ projects/shared-ui/src/lib/icons/solid/xmark/xmark.so-icon.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+    selector: 'dma-icon[dma-xmark-so-icon]',
+    templateUrl: './xmark.so-icon.svg',
+    styleUrl: '../../icon.css',
+    changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class XmarkSoIcon {}
