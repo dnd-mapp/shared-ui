ab86612e8ca4167f2c1f914055534eb66d1b500e
Oscar Wellner <oscar.wellner@gmail.com>
2026-03-06 19:19:35 +0100

    Docs: Update NavbarLink documentation

--- projects/shared-ui/src/stories/nav/navbar-link/default/default-story.component.ts
+++ projects/shared-ui/src/stories/nav/navbar-link/default/default-story.component.ts
@@ -2,13 +2,13 @@ import { ChangeDetectionStrategy, Component, input } from '@angular/core';
 import { NavbarLinkComponent } from '@dnd-mapp/shared-ui';
 
 @Component({
-    selector: 'dma-story',
-    templateUrl: './story.component.html',
-    styleUrl: './story.component.scss',
+    selector: 'dma-default-story',
+    templateUrl: './default-story.component.html',
+    styleUrl: './default-story.component.scss',
     changeDetection: ChangeDetectionStrategy.OnPush,
     imports: [NavbarLinkComponent],
 })
-export class StoryComponent {
+export class DefaultStoryComponent {
     public readonly label = input('My route');
 
     public readonly route = input('/my-route');
